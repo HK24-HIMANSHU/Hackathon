156245cef0cfe3c77fb51c81849bc941995890dd
Himanshu kumar <56072567+HK24-HIMANSHU@users.noreply.github.com>
2023-05-15 16:16:36 +0530

    Add files via upload

---
+++ Hackathon/Dicitionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApplication.Hakathon
+{
+    public class Dicitionary
+    {
+        public Dictionary<string, string> myDict = new Dictionary<string, string>();
+
+        public Dicitionary()
+        {
+            myDict.Add("apples", "banana");
+            myDict.Add("cloth", "mangoes");
+            myDict.Add("profit", "iot");
+        }
+
+        public void AddItem(string key, string value)
+        {
+            myDict.Add(key, value);
+        }
+    }
+}
